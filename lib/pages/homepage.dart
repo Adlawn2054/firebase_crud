147ab75bcb78065061af5f8bbf30587745bd4164
ains2054 <https://github.com/ains2054/pc_flutter>
2025-10-14 16:13:32 +0800

    updated the code sa firestore.dart

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crud_exam/services/firestore.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,7 +11,8 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-
+  
+  final FirestoreService firestoreService = FirestoreService();
   final TextEditingController textController = TextEditingController();
 
 //oopen dialog box to add a note para ini sa yadtung floating action bar
@@ -20,7 +23,17 @@ void openNotebox(){
       controller: textController,
     ),
     actions: [
-      ElevatedButton(onPressed: () {}, child: Text("Add"))
+      //button to save
+      ElevatedButton(onPressed: () {
+        //add a new note
+        firestoreService.addNote(textController.text);
+
+        //clear the text controller
+        textController.clear();
+
+        //close the box
+        Navigator.pop(context);
+      }, child: Text("Add"))
     ],
   ),
   );
@@ -31,6 +44,38 @@ void openNotebox(){
      appBar: AppBar(title: Center(child: Text("Noting")),),
      floatingActionButton: FloatingActionButton(onPressed: openNotebox,
      child: Icon(Icons.add),),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: firestoreService.getNotesStream(),
+        builder: (context, snapshot){
+          // if we have a data get all the docs
+          if(snapshot.hasData){
+            List noteList = snapshot.data!.docs;
+            //display the list
+            return ListView.builder(
+              itemCount: noteList.length,
+              itemBuilder: (context, index){
+                //get each individual doc 
+                DocumentSnapshot document = noteList[index];
+                String docID = document.id;
+                //get note from each doc
+                Map<String, dynamic> data=
+                  document.data() as Map<String, dynamic>;
+                String noteText = data['note'];
+
+                // displa as a list tile 
+                return ListTile(
+                  title: Text(noteText),
+                );
+              }
+            );
+          }
+          
+          // if there is no data return nothing
+          else{
+            return const Text("No notes ");
+          }
+        },
+      ),
     );
   }
 }
--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,12 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService{
+  
   final CollectionReference notes = FirebaseFirestore.instance.collection("notes");
 
+//create or add a new note
   Future<void> addNote(String note){
     return notes.add({
       'note': note,
       'timestamp': Timestamp.now(),
     });
   }
+  // read the notes
+  Stream<QuerySnapshot> getNotesStream(){
+    final notesStream = 
+      notes.orderBy('timestamp', descending: true).snapshots(); 
+    
+    return notesStream;
+  }
+  // update
+  Future<void> updateNote(String docID, String newNote){
+    return notes.doc(docID).update({
+      'note': newNote,
+      'timestamp': Timestamp.now()
+    });
+  }
+  // delete
 }
